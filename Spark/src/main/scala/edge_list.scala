7bc9fd465984e68dedf34c739c9566639e75f0ea
Justin Cano <jcano001@ucr.edu>
2015-06-12 18:00:48 +0000

    map edgeList and vertices

--- Spark/src/main/scala/edge_list.scala
+++ Spark/src/main/scala/edge_list.scala
@@ -1,6 +1,8 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.graphx._
+import scala.util.control.NonFatal
 
 object edge_list {
     def main(args: Array[String]) {
@@ -12,7 +14,7 @@ object edge_list {
         val file_name = "hdfs://ip-172-31-10-101:9000/common-crawl/crawl-data/CC-MAIN-2015-18/segments/1429246633512.41/warc/warc-edges-00000"
 
         // function to hash "(src_url, dst_url)" to integers
-        def hash_record(record: String): String = {
+        def hashRecord(record: String): String = {
             val error = "error".hashCode.toString
             val r = record.split(", ")
             // Catch ArrayIndexOutOfBoundsException
@@ -25,15 +27,37 @@ object edge_list {
             }
         }
 
+        // function to map src_url to its hash integer
+        def mapVertexHash(record: String): (Int, String) = {
+            val error = "error".hashCode
+            val r = record.split(", ")
+            // Catch ArrayIndexOutOfBoundsException
+            try {
+                val src_url = r(0).replace("(", "")
+                (src_url.hashCode, src_url)
+            } catch {
+                case NonFatal(exc) => (error, "error")
+            }
+        }
 
         // read in the data from HDFS
         val rdd = sc.textFile(file_name)
 
         // map each record into a tuple consisting of the hash codes of (src_url, dst_url)
-        val vertices = rdd.map(record => hash_record(record))
+        val edgeList = rdd.map(record => hashRecord(record))
 
         // save the data back into HDFS
-        val output_file_name = "hdfs://ip-172-31-10-101:9000/data/edge-lists/edge-list-file-0000"
-        vertices.saveAsTextFile(output_file_name)
+        val edgeListFileName = "hdfs://ip-172-31-10-101:9000/data/edge-lists/edge-list-file-0000"
+        edgeList.saveAsTextFile(edgeListFileName)
+
+        // map each VertexName to its VertexId
+        val vertices = rdd.map(record => mapVertexHash(record))
+        val verticesFileName = "hdfs://ip-172-31-10-101:9000/data/vertices/vertices-file-0000"
+
+        // Setup GraphX graph
+        val graph = GraphLoader.edgeListFile(sc, edgeListFileName)
+        // Run PageRank
+        val ranks = graph.pageRank(0.0001).edgeList
+
     }
 }
