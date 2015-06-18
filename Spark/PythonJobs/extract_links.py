b7dab66c2cad9f750313304dfc1f3c207c31e3dc
Justin Cano <jcano001@ucr.edu>
2015-06-18 22:45:23 +0000

    Delete existing hdfs:///data/link-edges if necessary

--- Spark/PythonJobs/extract_links.py
+++ Spark/PythonJobs/extract_links.py
@@ -46,6 +46,8 @@ def main():
     link_edges = warc_paths.map(extract_links)
     print link_edges.count()
     print link_edges.take(10)
+    # Delete existing /data/link-edges directory...
+    os.system('hdfs dfs -rm -r -f /data/link-edges')
     link_edges.saveAsTextFile('hdfs:/ip-172-31-10-101:9000/data/link-edges')
 
 if __name__ == '__main__':
