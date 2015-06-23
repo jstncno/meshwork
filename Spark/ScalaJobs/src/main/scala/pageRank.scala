73ff0e96395d31a935c8aa978d8355c49f887806
Justin Cano <jcano001@ucr.edu>
2015-06-23 01:19:30 +0000

    explicitly state HBase cluster in the config

--- Spark/ScalaJobs/src/main/scala/pageRank.scala
+++ Spark/ScalaJobs/src/main/scala/pageRank.scala
@@ -76,6 +76,8 @@ object pageRank {
         // Store ranks to HBase
         def putInHBase(vertex: (String, Double)): Unit = {
             val hbaseConf = HBaseConfiguration.create()
+            hbaseConf.set("hbase.zookeeper.quorum", "ec2-52-8-87-99.us-west-1.compute.amazonaws.com")
+            hbaseConf.set("hbase.zookeeper.property.clientPort", "2181")
             val tableName = "websites"
             val table = new HTable(hbaseConf, tableName)
             val vertexId = md5(vertex._1).toString
