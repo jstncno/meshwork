093955ab5eeded7efa08118444545f467e91e593
Justin Cano <jcano001@ucr.edu>
2015-06-24 02:25:59 +0000

    testing reactjs with components for vertices

---
+++ js/vertex-components.js
@@ -0,0 +1,47 @@
+var VertexListContainer = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  render: function() {
+    return (
+      <div className="vertexListContainer">
+        <VertexList data={this.state.data} />
+      </div>
+    );
+  }
+});
+
+var VertexList = React.createClass({
+  render: function() {
+    var vertexNodes = this.props.data.map(function (vertex) {
+      return (
+        <Vertex url={vertex.url}>
+          {vertex.pageRank}
+        </Vertex>
+      );
+    });
+    return (
+      <div className="vertexList">
+        {vertexNodes}
+      </div>
+    );
+  }
+});
+
+var Vertex = React.createClass({
+  render: function() {
+    return (
+      <div className="vertex">
+        <h2 className="vertexUrl">
+          {this.props.url}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+React.render(
+  <VertexListContainer url="vertices.json" />,
+  document.getElementById('content')
+);
