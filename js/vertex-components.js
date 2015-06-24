d7ec0df2f9efac301956398fe37b9c466ec737ac
Justin Cano <jcano001@ucr.edu>
2015-06-24 02:39:50 +0000

    use ajax to get data from vertices.json

--- js/vertex-components.js
+++ js/vertex-components.js
@@ -1,11 +1,24 @@
 var VertexListContainer = React.createClass({
-  /*getInitialState: function() {
+  getInitialState: function() {
     return {data: []};
-  },*/
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function() {
     return (
       <div className="vertexListContainer">
-        <VertexList data={this.props.data} />
+        <VertexList data={this.state.data} />
       </div>
     );
   }
@@ -42,6 +55,6 @@ var Vertex = React.createClass({
 });
 
 React.render(
-  <VertexListContainer data="vertices.json" />,
+  <VertexListContainer url="vertices.json" />,
   document.getElementById('content')
 );
