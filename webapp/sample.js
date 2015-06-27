70b0eaba34c58d94f31e9af9552ed1ed2fe0a51d
Justin Cano <jcano001@ucr.edu>
2015-06-27 01:24:32 +0000

    removed unnecessary nodejs files

--- webapp/sample.js
+++
@@ -1,8 +0,0 @@
-  var assert = require('assert');
-  var hbase = require('hbase');
-  
-  hbase({ host: '127.0.0.1', port: 10001 })
-  .table( 'websites' )
-  .schema(function(error, schema){
-    console.log(schema);
-  });
